edc076b2a29d99cc659ab231cbdcb4ae65c0d68a
QingpingMeng <missing1989@gmail.com>
2016-09-08 23:25:47 -0700

    adding server error directive

--- client/controllers/login.js
+++ client/controllers/login.js
@@ -1,7 +1,32 @@
 var app = angular.module('Instagram');
 
-var LoginCtrl = function(){
+var LoginCtrl = function($rootScope, $auth, $window){
 	var vm = this;
-}
 
+	vm.facebookSignin = function(){
+		$auth.authenticate('facebook')
+		.then(function(res){
+			$window.localStorage.currentUser = JSON.stringify(res.data.user);
+			$rootScope.currentUser = res.data.user;
+		});
+	};
+
+
+	vm.emailLogin = function(){
+		$auth.login({email: vm.email, password: vm.password})
+		.then(function(response){
+			$window.localStorage.currentUser = JSON.stringify(response.data.user);
+			$rootScope.currentUser = response.data.user;
+		})
+		.catch(function(res){
+			$scope.errorMessage={};
+			angular.forEach(res.data.message, function(message, field){
+				vm.loginForm[field].$setValidity('server',false);
+				vm.errorMessage[field] = res.data.message[field];
+			});
+		});
+	};
+};
+
+LoginCtrl.$inject=['$rootScope','$auth','$window'];
 app.controller('LoginCtrl',LoginCtrl);
